bc38a5fcee36233b584a447ac36a992d4076ec7d
Gary Struthers <agilej2ee@earthlink.net>
2012-11-20 17:02:25 -0800

    add ch 6 enums

---
+++ effective-java/src/main/java/ch/six/Herb.java
@@ -0,0 +1,17 @@
+package ch.six;
+
+
+public class Herb {
+	public enum Type { ANNUAL, PERENNIAL, BIENNIAL }
+	private final String name;
+	private final Type type;
+	
+	Herb(String name, Type type) {
+		this.name = name;
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return name;
+	}
+}
