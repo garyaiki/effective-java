f034c4412178e9748e1d45f8f1a9d9b35153d5b4
Gary Struthers <agilej2ee@earthlink.net>
2012-11-24 17:17:31 -0800

    finish ch 8

---
+++ effective-java/src/main/java/ch/eight/PreferInterfacesToReflection.java
@@ -0,0 +1,13 @@
+package ch.eight;
+/* Item 53
+ * Reflection bypasses typechecking, code is ugly, usually slower
+ * 
+ * As a rule don't use reflection at runtime
+ */
+/*
+ * Guava TypeToken retains type information
+ * @see ch.five.TypesafeHeterogeneousContainers
+ */
+public class PreferInterfacesToReflection {
+
+}
---
+++ effective-java/src/main/java/ch/eight/StringBuilderNotConcatenation.java
@@ -0,0 +1,7 @@
+package ch.eight;
+/* Item 51
+ * 
+ */
+public class StringBuilderNotConcatenation {
+
+}
